6797b13251fc1a901268fe2458a2ffce43627605
pabloa010 <pablo.aburto@live.com>
2024-02-03 09:56:25 -0600

    Ejemplo uso de funciones

---
+++ Practica_S3.py
@@ -0,0 +1,7 @@
+import os
+os.system("cls || clear")
+def get_full_name(name, last_name):
+    full_name = name + ' ' + last_name
+    return full_name
+print(f"Hola {get_full_name ('Pablo', 'Aburto')}")
+
