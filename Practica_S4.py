b767f5fa997e726939aef9e72df5314d2cd2f605
pabloa010 <pablo.aburto@live.com>
2024-02-10 09:20:15 -0600

    Session_4

---
+++ Practica_S4.py
@@ -0,0 +1,29 @@
+class Persona:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+
+    def saludar(self):
+        print(f"Hola, soy {self.nombre} y tengo {self.edad} años.")
+        
+# crear objeto persona
+persona1 = Persona("Durán", 79)
+persona1.saludar()
+
+esposa = Persona("Gabriela",29)
+esposa.saludar()
+
+
+
+class Estudiante(Persona):
+    def __init__(self, nombre, edad, grado):
+        super().__init__(nombre, edad)
+        self.grado = grado
+
+    def estudiar(self):
+        print(f"{self.nombre} está estudiando el grado {self.grado}.")
+
+
+estudiante1 = Estudiante("Gabriela", 29, "Maestría")
+estudiante1.saludar()
+estudiante1.estudiar()
