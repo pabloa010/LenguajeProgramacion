5d7c5073bc881820e1a4dd625990119163411ac6
pabloa010 <pablo.aburto@live.com>
2024-02-17 09:08:21 -0600

    Update(Tipos de listas)

--- Ejemplo3.py
+++ Ejemplo3.py
@@ -1,1 +1,11 @@
 #listas
+lista = [1, 2, 3, "Manzana", 15.6, True]
+print(lista)
+
+#Imprimir primer elemento de la lista
+print(lista[0])
+#Tamaño de la lista
+tam = len(lista)
+print(f"el tamaño es {tam}")
+#Imprimir el ultimo valor
+print(lista[tam-1])
