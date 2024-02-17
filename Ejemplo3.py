aea4cf7746780422aa3cc52ed8de918e9fae4375
pabloa010 <pablo.aburto@live.com>
2024-02-17 09:50:32 -0600

    Ejemplos clases

--- Ejemplo3.py
+++ Ejemplo3.py
@@ -9,3 +9,28 @@ tam = len(lista)
 print(f"el tamaño es {tam}")
 #Imprimir el ultimo valor
 print(lista[tam-1])
+
+#Imprimir los primeros 3 elementos
+print(lista[0:3])
+
+#Imprimir los elementos 4 hasta 6
+print(lista[3:7])
+
+#Agregar un elemento a la lista
+lista.append("Aburto")
+print(lista)
+
+#Imprimir los ultimos 3 elementos de la lista
+final = len(lista)
+inicio = final - 3
+print(lista[inicio: final +1])
+
+#Eliminar manzana
+lista.remove("Manzana")
+lista.remove("Aburto")
+
+#Ordenar lista
+lista.sort()
+print(lista)
+
+
---
+++ Sesion 5/Proyecto/classes.py
@@ -0,0 +1,11 @@
+class Student:
+    def__init__(self, id, name, major)
+        self.id = id
+        self.name = name
+        self.major = major
+    
+    def __str__(self):
+        return f"Nombre: {self.name}\nCarrera: {self.major}"
+    
+erving = Student(1, "Erving Artola", "ISI")
+print(erving)
